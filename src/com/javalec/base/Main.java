71e6af245bce61aa4c49ef4759d59a6e26872baf
leebyoungjun <21dlqudwns@gmail.com>
2022-09-18 22:37:22 +0900

    첫번째

---
+++ src/com/javalec/base/Main.java
@@ -0,0 +1,42 @@
+package com.javalec.base;
+
+import java.util.Scanner;
+
+import com.javalec.funtion.Calc;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		int getNum1 =0;
+		int getNum2 =0;
+		
+		System.out.println("첫번쨰 숫자를 입력하세요 :");
+		getNum1 = scanner.nextInt();
+
+		System.out.println("두번째 숫자를 입력하세요 :");
+		getNum2 = scanner.nextInt();
+		
+
+//		Calc calc = new Calc(getNum1, getNum2);
+
+		
+		//----------------빈생성자 또는 하나만받는생성자사용할때 테스트용.
+		
+		//		Calc calc1 = new Calc(getNum1);
+//		
+//		calc1.sum();
+//		
+//		System.out.println(calc1.sum());
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/com/javalec/function/Calc.java
@@ -0,0 +1,61 @@
+package com.javalec.function;
+
+public class Calc {
+	int num1 =0;
+	int num2 =0;
+	int divMok=0,divNam=0;
+	
+	
+//	public Calc() {
+//		// TODO Auto-generated constructor stub
+//	}//num하나도몰라도 넌 calc 오브젝을만들수가있어 나중에주던지
+//	
+//	public Calc(int num1) {
+//		super();
+//		this.num1 = num1;
+//		// TODO Auto-generated constructor stub
+//	}//넌 일단 숫자하나만알아도 사용가능해 ㅇ
+
+	public Calc(int num1, int num2) {
+		super();
+		this.num1 = num1;
+		this.num2 = num2;
+		
+		System.out.println(">>덧셈결과 :"+sum());
+		System.out.println(">>뺄셈결과 :"+sub());
+		System.out.println(">>곱셈결과 :"+multi());
+		System.out.println(">>나눗셈 몫 :"+div());
+		
+		
+	}//완성형
+	
+	public int sum() {	
+		 return num1+num2;
+	}
+	
+	public int sub() {
+		return num1 - num2;
+	}
+	
+	public int multi() {
+		return num1*num2;
+	}
+//	public int div() {
+//		
+//		divMok = num1/num2;
+//		divNam = num1-(num2*divMok);
+//		return divMok;
+//		
+//	}
+	public int div() {
+		
+		return num1/num2;
+			
+	}
+	
+	
+	//------------------get할수잇는 메소드만들기(만약 하나만받을 수 있는 상태라다면)
+	
+	
+	
+}
