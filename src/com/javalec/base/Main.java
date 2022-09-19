eba7879718877559301428e999259121e828bb45
junbbbb <113100170+junbbbb@users.noreply.github.com>
2022-09-19 16:03:10 +0900

    시작

--- src/com/javalec/base/Main.java
+++ src/com/javalec/base/Main.java
@@ -10,31 +10,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
 		
-		//고석진 첫번째수정
-		int getNum1 =0;
-		int getNum2 =0;
-		//고석진 수정2
-		//임은빈 확인완
-		//김성진 확인완료
-		//이예 띤 확인
-		
-		System.out.println("첫번쨰 숫자를 입력하세요 :");
-		getNum1 = scanner.nextInt();
-
-		System.out.println("두번째 숫자를 입력하세요 :");
-		getNum2 = scanner.nextInt();
-		
-
-		Calc calc = new Calc(getNum1, getNum2);
-
-		
-		//----------------빈생성자 또는 하나만받는생성자사용할때 테스트용.
-		
-		//		Calc calc1 = new Calc(getNum1);
-//		
-//		calc1.sum();
-//		
-//		System.out.println(calc1.sum());
+		System.out.println("1.덧셈\n2.뺄셈\n3.");
 		
 		
 		
