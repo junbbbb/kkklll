175def35642e5be253571fcb00e48fa5de8554d5
Moortje <kbc617@hotmail.com>
2022-09-19 16:14:13 +0900

    곱셈 업데이트 김성진

---
+++ src/com/javalec/function/Multiplication.java
@@ -0,0 +1,20 @@
+package com.javalec.function;
+
+public class Multiplication {
+
+	// Fields
+	int result = 0; // 곱셉 결과
+
+	// Constructor
+	public Multiplication() { // 생성자
+
+	}
+
+	// Method
+	public int Multiplication(int num1, int num2) { // 곱셉 연산
+		result = num1 * num2;
+		return result;
+
+	}
+
+}
