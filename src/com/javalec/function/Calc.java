9c6627d8b4930a1470d6bf1dc4243a77a7467ffa
Bhan <supersounds@naver.com>
2022-09-19 16:16:09 +0900

    addition1

--- src/com/javalec/function/Calc.java
+++ src/com/javalec/function/Calc.java
@@ -1,7 +1,19 @@
 package com.javalec.function;
 
 public class Calc {
+	//Field
+	int getNum = 0;
+	int num1, num2;
 	
+	public Calc() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void addition() {
+        if(getNum==1) {
+        	System.out.println("덧셈 결과는 " + (num1+num2)+ "입니다.");
+        	}
+			}
 	
 	
 }
